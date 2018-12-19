8d5b1b166c4b99e69a95e1922cd7a2d38b125615
Philip Scott <philip.scott@cantabcapital.com>
2018-12-19 16:36:09 +0000

    add some inspinia stuff

---
+++ src/components/IssueTracker.vue
@@ -0,0 +1,15 @@
+<template>
+    <h1>Cantab code review system</h1>
+</template>
+
+<script>
+export default {
+  name: "IssueTracker",
+  props: {
+    msg: String
+  }
+};
+</script>
+
+<style>
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <IssueTracker msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import IssueTracker from "@/components/IssueTracker.vue";
 
 export default {
   name: "home",
   components: {
-    HelloWorld
+    IssueTracker
   }
 };
 </script>
